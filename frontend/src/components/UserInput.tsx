db3c6f26ca16a053f0d2ac53b2a4678872f304e7
Dominika Iza Kowalska <dominiik@stud.ntnu.no>
2022-10-18 12:47:08 +0200

    Created filer dropdown box

--- frontend/src/components/UserInput.tsx
+++ frontend/src/components/UserInput.tsx
@@ -1,6 +1,10 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 
 function Search() {
@@ -15,6 +19,21 @@ function Search() {
                 <Button variant="contained" >
                     Search
                 </Button>
+
+                <FormControl sx={{ ml: "10px", minWidth: 120 }}>
+                    <InputLabel id="dropdown-menu" size='small'>Filter</InputLabel>
+                    <Select
+                        labelId="dropdown-menu"
+                        id="select-search-filter"
+                        label="Filter"
+                        size='small'
+                    >
+                        <MenuItem value=""> <em>None</em> </MenuItem>
+                        <MenuItem value="filter1" >filter1</MenuItem>
+                        <MenuItem value="filter1" >filter2</MenuItem>
+                        <MenuItem value="filter1" >filter2</MenuItem>
+                    </Select>
+                </FormControl>
             </Box>
         </div>
     );
