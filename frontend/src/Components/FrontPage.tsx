57a50229c864cfed0c9d790a32796c01b6915cd5
Eivind Dalholt <eividalh@stud.ntnu.no>
2022-10-19 11:46:58 +0200

    fetch example for songs

--- frontend/src/Components/FrontPage.tsx
+++ frontend/src/Components/FrontPage.tsx
@@ -1,27 +1,36 @@
-import React from "react";
-import { useQuery, gql } from '@apollo/client';
+import React, { useEffect, useState } from "react";
+import { useQuery, gql, ApolloError } from '@apollo/client';
 
-function FrontPage() {
+const FrontPage = () => {
 
 const GET_SONGS = gql`
   query {
     getSongs{
-        page
-        totalPages
+      songs{
+        name
+        artists
+        danceability
+        year
       }
+      page
+      totalPages
+    }
   }
 `;
-
   const { loading, error, data } = useQuery(GET_SONGS);
-  
-
   console.log(data)
-  if (loading) return <>null</>;
+  
+  if (loading) return <>Loading</>;
   if (error) return <>error</>;
     return (
         <>
-        front
-        {data.totalPages}
+        <h2>Spotify explorer</h2>
+        <ul>
+        {data.getSongs.songs.map((song: {name: String, year: number}, i: number) =>
+        <li key={i}><b>{song.name}</b> released in year {song.year} </li>
+        )}
+        </ul>
+        Page {data.getSongs.page} of {data.getSongs.totalPages}
         </>
     );
   }
