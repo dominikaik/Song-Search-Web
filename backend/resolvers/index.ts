07787a691ecb2eef8e6f3ef776e4c8e0f1e659de
Eivind Dalholt <eividalh@stud.ntnu.no>
2022-10-20 15:38:24 +0200

    one liner

--- backend/resolvers/index.ts
+++ backend/resolvers/index.ts
@@ -9,7 +9,7 @@ module.exports = {
       const page = args.page || 1
       const limit = args.pageSize || 10
       const orderBy = args.orderBy || {}
-      //Search for string if there is any, else return regex to match all.
+      // Search for string if there is any, else return regex to match all.
       const search: string | RegExp = args.search || /.*/
 
       const searchQuery: searchQueryType = {
@@ -18,15 +18,15 @@ module.exports = {
           {artists: new RegExp(search, 'i')}
         ],
       };
-      //Add year if requested
+      // Add year if requested
       (args.year) ? (searchQuery["year"] = args.year) : (null)
 
-      //Fetch from DB
+      // Fetch from DB
       const songsFetched = await Song.find(searchQuery).limit(limit).skip((page-1)*limit).sort(orderBy)
       const count: number = await Song.find(searchQuery).count();
       const totalPages: number = Math.ceil(count / limit)
   
-      //Return data by format defined in schema
+      // Return data by format defined in schema
       return {
         songs: songsFetched,
         page: page,
@@ -37,13 +37,12 @@ module.exports = {
     }
   },
 
-  //Connected to mutation for song rating.
+  // Connected to mutation for song rating.
   rateSong: async (args: rateSongArgs) => {
     try {
-      const id = args._id
-      const rating = args.rating
-      //Manipulates a specific song by id, returns new document.
-      return await Song.findByIdAndUpdate(id, {rating: rating}, {returnOriginal: false})
+      // Manipulates a specific song by id, returns new document.
+      // https://mongoosejs.com/docs/api.html
+      return await Song.findByIdAndUpdate(args._id, {rating: args.rating}, {returnOriginal: false})
     } catch (error) {
       throw error
     }
