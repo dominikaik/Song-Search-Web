90e53ea613ff9e4ada168af17209918168f9f3fa
Dominika Iza Kowalska <dominiik@stud.ntnu.no>
2022-10-27 12:32:01 +0200

    Created render tests for sorting bars

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pageTitle = screen.getByText(/Spotify explorer/i);
+  expect(pageTitle).toBeInTheDocument();
 });
--- frontend/src/__tests__/UserInput.test.tsx
+++ frontend/src/__tests__/UserInput.test.tsx
@@ -1,19 +1,33 @@
-import Search from '../components/UserInput'
+import FrontPage from '../components/FrontPage'
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import App from '../App';
 
 test('renders search box', () => {
-    render(<Search />);
-    const searchElement = screen.getByLabelText(/What are you searching for?/i);
+    render(<App />);
+    const searchElement = screen.getByLabelText(/Search for a song or artist/i);
     expect(searchElement).toBeInTheDocument();
   });
 
-describe('Input value', () => {
+test('renders sort-by box', () => {
+  render(<App />);
+  const sortByElement = screen.getByLabelText(/Sort by/i);
+  expect(sortByElement).toBeInTheDocument();
+});
+
+test('renders order-by box', () => {
+  render(<App />);
+  const orderByElement = screen.getByLabelText(/Order by/i);
+  expect(orderByElement).toBeInTheDocument();
+});
+
+
+/* describe('Input value', () => {
     it('updates on change', () => {
       const setSearch = jest.fn((value) => {})
       
-      const { queryByPlaceholderText } = render(<Search setSearch={setSearch}/>)
+      const { queryByPlaceholderText } = render(<FrontPage setSearch={setSearch}/>)
   
       const searchInput = queryByPlaceholderText('Search...')
   
@@ -21,4 +35,4 @@ describe('Input value', () => {
   
       expect(searchInput.value).toBe('test')
     })
-})
+}) */
